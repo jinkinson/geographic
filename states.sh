c71f901aaf797f4f6f5ad2e40c5d217c65edda66
jinkinson <43326541+jinkinson@users.noreply.github.com>
2020-05-07 21:59:42 -0400

    Add files via upload
    This script is a Bash script that serves to convert US state abbreviations for the 50 states and DC into their full names (e.g. AL->Alabama, AK->Alaska, etc.).

---
+++ states.sh
@@ -0,0 +1,52 @@
+sed 's/AK/Alaska/g' $1 > output.txt
+sed 's/AL/Alabama/g' output.txt > output2.txt
+sed 's/AZ/Arizona/g' output2.txt > output3.txt
+sed 's/AR/Arkansas/g' output3.txt > output4.txt
+sed 's/CA/California/g' output4.txt > output5.txt
+sed 's/CO/Colorado/g' output5.txt > output6.txt
+sed 's/CT/Connecticut/g' output6.txt > output7.txt
+sed 's/DC/District of Columbia/g' output7.txt > output8.txt
+sed 's/DE/Delaware/g' output8.txt > output9.txt
+sed 's/FL/Florida/g' output9.txt > output10.txt
+sed 's/GA/Georgia/g' output10.txt > output11.txt
+sed 's/HI/Hawaii/g' output11.txt > output12.txt
+sed 's/IA/Iowa/g' output12.txt > output13.txt
+sed 's/ID/Idaho/g' output13.txt > output14.txt
+sed 's/IL/Illinois/g' output14.txt > output15.txt
+sed 's/IN/Indiana/g' output15.txt > output16.txt
+sed 's/KS/Kansas/g' output16.txt > output17.txt
+sed 's/KY/Kentucky/g' output17.txt > output18.txt
+sed 's/LA/Louisiana/g' output18.txt > output19.txt
+sed 's/MA/Massachusetts/g' output19.txt > output20.txt
+sed 's/MD/Maryland/g' output20.txt > output21.txt
+sed 's/ME/Maine/g' output21.txt > output22.txt
+sed 's/MI/Michigan/g' output22.txt > output23.txt
+sed 's/MN/Minnesota/g' output23.txt > output24.txt
+sed 's/MO/Missouri/g' output24.txt > output25.txt
+sed 's/MS/Mississippi/g' output25.txt > output26.txt
+sed 's/MT/Montana/g' output26.txt > output27.txt
+sed 's/NC/North Carolina/g' output27.txt > output28.txt
+sed 's/ND/North Dakota/g' output28.txt > output29.txt
+sed 's/NE/Nebraska/g' output29.txt > output30.txt
+sed 's/NH/New Hampshire/g' output30.txt > output31.txt
+sed 's/NJ/New Jersey/g' output31.txt > output32.txt
+sed 's/NM/New Mexico/g' output32.txt > output33.txt
+sed 's/NV/Nevada/g' output33.txt > output34.txt
+sed 's/NY/New York/g' output34.txt > output35.txt
+sed 's/OH/Ohio/g' output35.txt > output36.txt
+sed 's/OK/Oklahoma/g' output36.txt > output37.txt
+sed 's/OR/Oregon/g' output37.txt > output38.txt
+sed 's/PA/Pennsylvania/g' output38.txt > output39.txt
+sed 's/RI/Rhode Island/g' output39.txt > output40.txt
+sed 's/SC/South Carolina/g' output40.txt > output41.txt
+sed 's/SD/South Dakota/g' output41.txt > output42.txt
+sed 's/TN/Tennessee/g' output42.txt > output43.txt
+sed 's/TX/Texas/g' output43.txt > output44.txt
+sed 's/UT/Utah/g' output44.txt > output45.txt
+sed 's/VA/Virginia/g' output45.txt > output46.txt
+sed 's/VT/Vermont/g' output46.txt > output47.txt
+sed 's/WA/Washington/g' output47.txt > output48.txt
+sed 's/WI/Wisconsin/g' output48.txt > output49.txt
+sed 's/WV/West Virginia/g' output49.txt > output50.txt
+sed 's/WY/Wyoming/g' output50.txt > final.txt
+rm output*
